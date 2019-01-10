cd530e93f7753a5f05a79b58cea630109d28a8f1
Megan Farley <megfarley4@gmail.com>
2019-01-10 17:12:49 -0600

    added some notes

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::ApplicationController < ApplicationController
 
   def check_authentication
+    #UPDATE THIS
     render json: {error: 'Please log in' }, status: 401
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 
   has_secure_password
+  
   has_one :business, dependent: :destroy
   has_many :messages, dependent: :destroy
   has_many :bills, dependent: :destroy
